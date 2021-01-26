7cb93e9a3c9c506c5492fb3a44abcde295bae914
CanftIn <wwc7033@gmail.com>
2021-01-27 04:10:42 +0800

    update linq

--- boolinq/boolinq.h
+++ boolinq/boolinq.h
@@ -42,6 +42,66 @@ public:
     T next() {
         return nextFunc(storage);
     }
+
+    void for_each_i(std::function<void(T)> apply) const {
+        Linq<S, T> linq = *this;
+        try {
+            while (true) {
+                apply(linq.next());
+            }
+        } catch (LinqEndException &) {}
+    }
+
+    void for_each(std::function<void(T)> apply) const {
+        return for_each_i([apply](T value) { return apply(value); });
+    }
+
+    Linq<std::tuple<Linq<S, T>, int>, T> where_i(std::function<bool(T, int)> filter) const {
+        return Linq<std::tuple<Linq<S, T>, int>, T>(
+            std::make_tuple(*this, 0),
+            [filter](std::tuple<Linq<S, T>, int>& tuple) {
+                Linq<S, T>& linq = std::get<0>(tuple);
+                int& index = std::get<1>(tuple);
+
+                while (true) {
+                    T ret = linq.next();
+                    if (filter(ret, index++))
+                        return ret;
+                }
+            }
+        );
+    }
+
+    Linq<std::tuple<Linq<S, T>, int>, T> where(std::function<bool(T)> filter) const {
+        return where_i([filter](T value, int) { return filter(value); });
+    }
+
+    bool any(std::function<bool(T)> predicate) const {
+        Linq<S, T> linq = *this;
+        try {
+            while (true) {
+                if (predicate(linq.next()))
+                    return true;
+            }
+        } catch (LinqEndException &) {}
+        return false;
+    }
+
+    bool any() const {
+        return any([](T value) { return static_cast<bool>(value); });
+    }
+
+    bool all(std::function<bool(T)> predicate) const {
+        return !any([predicate](T value) { return !predicate(value); });
+    }
+
+    bool all() const {
+        return all([](T value) {return static_cast<bool>(value); });
+    }
+
+    bool contains(const T& item) const {
+        return any([&item](T value) { return value == item; });
+    }
 };
 
 template <typename T>
@@ -54,6 +114,18 @@ from(const T& begin, const T& end) {
             return *(pair.first++);
         }
     );
+}
+
+template <typename T>
+Linq<std::pair<T, T>, typename std::iterator_traits<T>::value_type>
+from(const T& it, int n) {
+    return from(it, it + n);
+}
+
+template <typename T, int N>
+Linq<std::pair<const T*, const T*>, T>
+from(T (&array)[N]) {
+    return from((const T*)(&array), (const T*)(&array) + N);
 }
 
 template <template <class, class> class TV, typename TT, typename TU>
--- boolinq/test.cpp
+++ boolinq/test.cpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <iostream>
 
 auto main() -> int {
     std::vector<int> src;
@@ -10,5 +11,18 @@ auto main() -> int {
     src.push_back(3);
 
     auto range = from(src);
+    std::cout << std::boolalpha << range.any() << std::endl;
+    std::cout << std::boolalpha << range.any([](int a){return a == 1;}) << std::endl;
+    std::cout << std::boolalpha << range.all([](int a){return a > 0;}) << std::endl;
+    std::cout << std::boolalpha << range.all([](int a){return a > 4;}) << std::endl;
+    std::cout << std::boolalpha << range.contains(1) << std::endl;
+    std::cout << std::boolalpha << range.contains(4) << std::endl;
+
+    std::cout << std::boolalpha << range.where([](int a){return a % 2 == 1;})
+                                   .any([](int a){return a == 2;}) << std::endl;
+
+    int num = 0;
+    from(src).for_each([&](int a){num += a;});
+    std::cout << num << std::endl;
     return 0;
 }
